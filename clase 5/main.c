b6ff5aabb42e2f8078e56c76fccbc95b4765393e
Ivanutn <ik_pache@hotmail.com>
2019-09-04 15:20:35 -0300

    clase 5 arrays

--- clase 5/main.c
+++ clase 5/main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define TOTAL 5
 int main()
 {
-    int total=5;
-    int vectorNumero[total];
+
+    int vectorNumero[TOTAL];
     int vector2[]={2,5,4,87,9};
-    for(int i=0;i<total;i++){
+    for(int i=0;i<TOTAL;i++){
         printf("Ingrese un numero:");
         scanf("%d",&vectorNumero[i]);
     }
 
     printf("numeros ingresados\n");
 
-    for(int i=0;i<total;i++){
+    for(int i=0;i<TOTAL;i++){
          printf("%3d\n",vectorNumero[i]);
     }
     printf("\nNumeros harcodeados.\n");
-    for(int i=0;i<total;i++){
+    for(int i=0;i<TOTAL;i++){
         printf("%3d\n",vector2[i]);
     }
 
