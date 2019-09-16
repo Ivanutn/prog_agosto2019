dbc8f77b3190f8f12468628e4d5a0ea2744ae7e8
alumno <alumno>
2019-09-16 08:42:10 -0300

    d

---
+++ BibliotecaUtn/utn.h
@@ -0,0 +1,5 @@
+
+int getInt(int* input,char message[],char eMessage[],int lowLimit,int highLimit);
+int getFloat(float* input,char message[],char eMessage[],float lowLimit,float highLimit);
+int getCharacter(char* input,char message[],char eMessage[],char lowChar,char highChar);
+int getString(char* input,char message[],char eMessage[],int lowLimit,int highLimit);
