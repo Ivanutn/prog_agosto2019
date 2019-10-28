642026fdf4129b37a5c215cd4dbe32557ac24d98
Ivanutn <ik_pache@hotmail.com>
2019-10-28 08:54:38 -0300

    agregada clase 17

---
+++ clase 17/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int id;
+    char nombre[20];
+    float sueldo;
+}eEmpleado
+int main()
+{
+    //array de punteros a empleados
+    //puntero auxiliar estatico que lo guarda en el array de dir de memorias que es dinamica.
+    //
+    eEmpleado* aux;
+
+
+    return 0;
+}
