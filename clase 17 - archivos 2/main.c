614a636c2e5602cb1e9ac260cc05490ba7d23af1
Ivanutn <ik_pache@hotmail.com>
2019-10-28 12:06:03 -0300

    archivos

--- clase 17 - archivos 2/main.c
+++ clase 17 - archivos 2/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+typedef struct
+{
+    int id;
+    char nombre[20];
+    float sueldo;
+}eEmpleado;
 
+int mostrarEmpleado(eEmpleado* e);
 int main()
 {
-    printf("Hello world!\n");
+    eEmpleado unEmpleado={1,"ana",15000};
+    FILE* f=fopen("data.bin","wb"); //modo escritura binaria
+    if(f==NULL)
+    {
+        printf("no se pudo crear un archivo.\n");
+    }
+    else
+    {
+
+    }
+
+    mostrarEmpleado(&unEmpleado);
     return 0;
+}
+int mostrarEmpleado(eEmpleado* e)
+{
+    int todoOK=0;
+    if(e!=NULL)
+    {
+        printf("%d  %s  %.2f\n",e->id,e->nombre,e->sueldo);
+    }
+    return todoOK;
 }
